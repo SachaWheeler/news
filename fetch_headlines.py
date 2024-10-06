535e552ef97d50067d753f988010d971f600e387
Sacha Wheeler <sacha@sachawheeler.com>
2024-10-06 10:22:07 +0100

    clean up

--- fetch_headlines.py
+++ fetch_headlines.py
@@ -2,34 +2,17 @@
 import requests
 import sqlite3
 from datetime import datetime
-from api_key import API_KEY
+# from api_key import API_KEY
 from pygooglenews import GoogleNews
 import re
 from utils import my_sources
 
-# Function to fetch headlines using NewsAPI
-"""
-def fetch_headlines():
-    api_key = API_KEY
-    url = "https://newsapi.org/v2/top-headlines"
-    params = {
-        'apiKey': api_key,
-        'category': 'general',
-        'language': 'en',
-        'pageSize': 100,  # Maximum number of results
-        # 'sources': 'bbc-news',
-        'page': 1,        # Page of results to fetch
-    }
-    response = requests.get(url, params=params)
-    return response.json()['articles']
-"""
 
 def fetch_headlines():
     gn = GoogleNews(lang='en', country='UK')  # You can change the language or country as needed
     # top_news = gn.topic_headlines('WORLD')  # Fetch top news
     top_news = gn.top_news()  # Fetch top news
 
-    # Extract the headlines and links from the returned feed
     articles = []
     for entry in top_news['entries']:
         title = entry.title
@@ -48,7 +31,6 @@ def headline_exists(c, title):
     c.execute("SELECT 1 FROM headlines WHERE title = ?", (title,))
     return c.fetchone() is not None
 
-# Function to save headlines to SQLite database
 def save_headlines_to_db(headlines):
     conn = sqlite3.connect('headlines.db')
     c = conn.cursor()
@@ -59,15 +41,15 @@ def save_headlines_to_db(headlines):
 
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-    # Insert headlines into the table, ignoring if already present (based on timestamp)
+    # Insert new headlines into the table
     for article in headlines:
         title = re.sub( r'[^A-Za-z0-9\s-]', '', article['title'].rpartition('-')[0].rstrip())
         if not headline_exists(c, title):
             publication = article['publication']
             print(f"adding '{title}' from {publication}")
-            c.execute('''INSERT OR IGNORE INTO headlines (title, publication, timestamp)
-                    VALUES (?, ?, ?)''',
-                    (title, publication, current_time))
+            c.execute('''INSERT OR IGNORE INTO headlines
+                (title, publication, timestamp) VALUES (?, ?, ?)''',
+                (title, publication, current_time))
 
     conn.commit()
     conn.close()
--- utils.py
+++ utils.py
@@ -1,18 +1,16 @@
 my_stopwords = [
-    'trump',
-    'biden',
+    'trump', 'biden',
     'rape',
     'us',
     'new',
     'jobs',
     'report',
-    'fantasy',
     'week',
     'says',
     'israel', 'israeli', 'jew',
-    'football',
-    'could',
-    'see',
+    'fantasy', 'football', 'rugby', 'league',
+    'could', 'might',
+    'see', 'man',
     'video',
 ]
 
